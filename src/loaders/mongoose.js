ae525ddd796c78e193195306cda2226e763ae7c3
DungBuiTien1999 <nguyentonhi199@gmail.com>
2021-05-18 20:58:21 +0700

    push len de hai fix cung

--- src/loaders/mongoose.js
+++ src/loaders/mongoose.js
@@ -1,17 +1,66 @@
+// "use strict";
+// Object.defineProperty(exports, "__esModule", { value: true });
+
 const mongoose = require("mongoose");
 
 const connectDB = async (url) => {
-  try {
-    await mongoose.connect(url, {
-      useCreateIndex: true,
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-    });
-    console.log("connected database");
-  } catch (err) {
-    console.error(err.message);
+
+  if(process.env.NODE_ENV ==='test') {
+    const Mockgoose = require('mockgoose').Mockgoose;
+    const mockgoose = new Mockgoose(mongoose);
+
+    mockgoose.prepareStorage()
+      .then(async () => {
+        try {
+          await mongoose.connect(url, {
+            useCreateIndex: true,
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+          });
+          console.log("connected database");
+        } catch (err) {
+          console.error(err.message);
+        }
+      })
+  } else {
+    try {
+      await mongoose.connect(url, {
+        useCreateIndex: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+      });
+      console.log("connected database");
+    } catch (err) {
+      console.error(err.message);
+    }
   }
 }
+// // exports.connectDB = connectDB;
+
+const closeDB = async () => {
+  return await mongoose.disconnect();
+}
+// exports.closeDB = closeDB;
 
 module.exports = connectDB;
+
+// module.exports = {
+//   async connectDB(url) {
+//     try {
+//       await mongoose.connect(url, {
+//         useCreateIndex: true,
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//         useFindAndModify: false,
+//       });
+//       console.log("connected database");
+//     } catch (err) {
+//       console.error(err.message);
+//     }
+//   },
+//   async closeDB() {
+//     return await mongoose.disconnect();
+//   }
+// };
